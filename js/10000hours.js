89ce132d3b8c95cc56939a7ce2f5c2a9eee18e7a
sunkyeongW <bluelight0949@naver.com>
2023-02-08 22:32:31 +0900

    learned about website creation using HTML, CSS, and JAVASCRIPT.

---
+++ js/10000hours.js
@@ -0,0 +1,69 @@
+const startButton= document.querySelector('.start_btn');
+const result = document.querySelector('.result');
+const model= document.querySelector('#model');
+const openButton= document.querySelector('.model_btn');
+const closeButton= document.querySelector('.close_btn');
+const shareButton= document.querySelector('.share_btn');
+const loading= document.querySelector('.result_loading');
+
+function calculator(){
+    const fieldValue = document.querySelector("#field_value");
+    const timeValue = (document.querySelector("#field_value"));
+    const timeValue_int = Number(timeValue.value);
+    const fieldResult = document.querySelector(".field_result");
+    const timeResult = document.querySelector(".time_result");
+
+    if(fieldValue.value ==""){
+        alert("분야가 입력되지 않았습니다.");
+        fieldValue.focus();
+        return false;}
+
+    else if(timeValue.vlaue ==""){
+        alert("시간이 입력되지 않았습니다.");
+        timeValue.focus();
+        return false;}
+    else if(timeValue_int > 24) {
+        alert("잘못된 값입니다. 24이하의 값을 입력해 주세요.");
+        return false;}
+
+    result.style.display = 'none';
+    loading.style.display = 'flex';
+
+    setTimeout(function(){
+        fieldResult.innerText = fieldValue.value;
+        timeResult.innerText = parseInt((10000/timeValue_int), 10);
+        loading.style.display = 'none';
+        result.style.display = 'flex';
+    },1800);
+
+    }
+
+
+function openModel(){
+    model.style.display = "flex";
+
+}
+
+function closeModel(){
+    model.style.display = "none";
+
+}
+
+window.onclick = function(event) {
+    if(event.target == model){
+        closeModel();
+}};
+
+function copyUrl(){
+    const url = window.location.href;
+
+    navigator.clipboard.writeText(url).then(() => {
+        alert("URL이 복사되었습니다."); 
+    });
+
+}
+
+shareButton.addEventListener('click',copyUrl);
+openButton.addEventListener('click',openModel);
+closeButton.addEventListener('click',closeModel);
+startButton.addEventListener('click',calculator);
